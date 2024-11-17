2fb1999b5794eb93e461a4c7f3f46237d5f63e17
Utkarsh <utkarshyadav0825@gmail.com>
2024-11-17 14:51:08 -0500

    added multiply

---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class mulyiply {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int multi = a*b;
+        System.out.println(multi);
+    }
+    
+}
